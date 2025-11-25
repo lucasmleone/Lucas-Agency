fa7f86b72dbc1151049a5b104dbf38e4b895605e
Lucas Leone <lucasmleone@gmail.com>
2025-11-25 15:31:36 -0300

    chore: increase rate limit to 1000

--- server/index.js
+++ server/index.js
@@ -27,7 +27,7 @@ const PORT = process.env.PORT || 3001;
 // Rate Limiting
 const limiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100, // limit each IP to 100 requests per windowMs
+    max: 1000, // limit each IP to 1000 requests per windowMs
     standardHeaders: true,
     legacyHeaders: false,
 });
